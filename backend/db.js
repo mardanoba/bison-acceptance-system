a9c60ffb3e7d54a55c007130291369cf5b6993ac
mardanoba <ktilahun706@gmail.com>
2026-01-18 00:27:11 +0300

    Fix port for Render

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,6 @@
 import mysql from "mysql2";
 import dotenv from "dotenv";
 
-// Load .env variables
 dotenv.config();
 
-export const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-   port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
-});
+export const db = mysql.createPool(process.env.MYSQL_URL);
--- backend/index.js
+++ backend/index.js
@@ -143,4 +143,8 @@ app.get("/", (req, res) => {
 app.get("/api/test", (req, res) => {
   res.json({ message: "Backend is working!" });
 });
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
