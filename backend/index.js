5b211f0c4c636c2548fbe468f729a96e5503ebd9
mardanoba <ktilahun706@gmail.com>
2026-01-17 20:23:23 +0300

    Add test API route

--- backend/index.js
+++ backend/index.js
@@ -140,5 +140,7 @@ app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.get("/", (req, res) => {
   res.json({ message: "Backend is working" });
 });
-
+app.get("/api/test", (req, res) => {
+  res.json({ message: "Backend is working!" });
+});
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
