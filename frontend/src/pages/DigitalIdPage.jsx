aa03ca6355171a99f3190da70105d0461f81b357
mardanoba <ktilahun706@gmail.com>
2026-01-20 22:31:45 +0300

    update DigitalIdPage

--- frontend/src/pages/DigitalIdPage.jsx
+++ frontend/src/pages/DigitalIdPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import logo from "../assets/b.webp";
+import logo from "../assets/fastway3.webp";
 
 function DigitalIdPage() {
   const { workId } = useParams();
